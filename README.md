435b552abc0e50f8501c710f74944aa6508fd209
luis.hincapie <luis.hincapie@blankfactor.com>
2024-02-13 14:46:56 -0500

    update documentation

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Altair - URL Shortener
 
+Altair is a URL shortener service that allows users to shorten long URLs into short and unique aliases. The service
+also provides a QR code for the short URL.
+
 ### How does a URL shortener work?
 
 At a high level, the URL shortener executes the following operations:
@@ -31,9 +34,10 @@ The reasons to shorten a URL are the following:
 - [x] Multiple users entering the same long URL must receive the same short URL (1-to-1 mapping)
 - [ ] URL expires after 30 days of not being use
 - [ ] Optional custom alias
-- [ ] QR generation
+- [x] QR generation
 
 ### Things to do
+
 - [x] Basic shortener algorithm
 - [x] Front end integration
 - [ ] Redis for cache
@@ -44,8 +48,81 @@ The reasons to shorten a URL are the following:
 - [ ] Documentation
 - [ ] Testing again
 - [ ] Cloud
- 
+
+### API
+
+#### Shorten URL
+
+```http
+POST /?longUrl={:url}
+```
+
+| Parameter | Type     | Description |
+| :-------- | :------- | :---------- |
+| `url`     | `string` | **Required**. The long URL to be shortened |
+
+Response
+
+```json
+{
+  "id": 10000,
+  "shortUrl": "http://localhost:8080/jO5Wi3",
+  "longUrl": "https://github.com/",
+  "createdAt": "2/13/24, 1:35 PM",
+  "expiresAt": "2024-03-13",
+  "accessCount": 0,
+  "qrCode": "iVBORw0KGgoAAAANSUhEUgAAAPoAAAD6AQAAAACgl2eQAAABU0lEQVR4Xu2XMRLCMAwExVBQ8gSekqeFp/kpeQJlCibm7uTMEBeUzHD4GsXKprAky0rUzyrRezoNIDWA1ABSA0h9DViDOtPMWF0Xra92AJ+3s9YBs0y70wzAzgE8bmU+cTXdERVboMJEXOofAFzbAlVVzXQLn3anF8Asc+cIwEajtR3QtMatiHvzOQF6o3Rj8eThpfMQKAegPnCjnvgmggZutio7gNrgxOGlIX6MgwWAZzSnDEB94tQWQoYA6jg4DaJH8WLNodAQCMVhYXEzAHdFxQ3Icr6wR+UZnrt0ewC8UVHOkDiadvF4ATi8bUxScWM2PLRiD6BJWc8e3FW1B6Cd808U4WjJ724cD4DPG39hprr3KEtg4Z9acopJTku2QOFsj/u1hcMS0OyLGOAMo0c5ApVVvXJakkwBbj4EsDkp3fmVFfBBA0gNIDWA1ABSvwG8AHdpNb7LjVzhAAAAAElFTkSuQmCC"
+}
+```
+
+#### Redirect URL
+
+```http
+GET /{:shortUrl}
+```
+
+| Parameter | Type     | Description |
+| :-------- | :------- | :---------- |
+| `shortUrl`     | `string` | **Required**. The short URL to be redirected |
+
+Response: Redirects to the original URL
+
+### Tech Stack
+
+#### Backend
+- Java 17
+- Spring Boot
+- H2 Database
+- JPA
+- Lombok
+- QRGen
+- Swagger
+- JUnit
+- Mockito
+- Docker
+- Docker Compose
+
+#### Frontend
+- React
+- Tailwind CSS
+- SWR
+- Next.js
+
+### How to run
+
+```bash
+docker-compose up
+```
+
+### How to use
+
+- Access the Swagger UI at http://localhost:8080/swagger-ui.html
+- Use the API to shorten a URL
+- Access the shortened URL to be redirected
+
+
 ### DB Schema
+
 ![ER-Diagram.png](.github%2Fmedia%2FER-Diagram.png)
 
 ### References
--- front end/components/shortenedUrl/ShortUrlCard.js
+++ front end/components/shortenedUrl/ShortUrlCard.js
@@ -9,12 +9,37 @@ export default function ShortUrlCard({urlVal}) {
     const handleCopyUrl = () => {
         navigator.clipboard.writeText(data.shortUrl).then(r => console.log("copied"));
     }
-    if (isValidating) return  <div>Loading...</div>
+    if (isValidating)
+        return <>
+            <div role="status" className="max-w-sm animate-pulse">
+                <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+                <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+                <div
+                    className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
+                    <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                        <path
+                            d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                    </svg>
+                </div>
+                <span className="sr-only">Loading...</span>
+            </div>
+        </>
+
     if (error) return <div>Error fetching data</div>
     return (
 
         <div>
-            <h2 className="text-2xl font-bold text-gray-800 my-2">Shortened URL</h2>
+            <h2 className="text-1xl font-semibold text-gray-800 my-2">Shortened URL</h2>
+            <h3 className="text-gray-500 text-sm">Created </h3>
+            <p className="text-gray-500 text-sm">{data.createdAt}</p>
+            <h3 className="text-gray-500 text-sm">Expires </h3>
+            <p className="text-gray-500 text-sm">{data.expiresAt}</p>
+
+            <h3 className="text-gray-500 text-sm">Clicks </h3>
+            <p className="text-gray-500 text-sm">{data.accessCount}</p>
+
             <div className="flex gap-4">
                 <Link href={data.shortUrl} passHref={true}>
                     <p className="text-blue-800 mt-2">{data.shortUrl}</p>
@@ -25,8 +50,7 @@ export default function ShortUrlCard({urlVal}) {
                 </button>
             </div>
             <div className="mt-4">
-                {/*<Image src={"data:image/png;base64, " + myState} alt="QR Code" width={200} height={200}/>*/}
-                <Image src={"data:image/png;base64, " +data.qrCode} alt="QR Code" width={200} height={200}/>
+                <Image src={"data:image/png;base64, " + data.qrCode} alt="QR Code" width={200} height={200}/>
             </div>
         </div>
     );
